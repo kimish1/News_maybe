1f9eabe7e6f69967932e48ade99fedd9ae9c28ee
Puzenko Igor <bjiast.001@gmail.com>
2025-08-07 14:46:20 +0300

    news kimish last1

--- src/fixtures/newsdata.ts
+++ src/fixtures/newsdata.ts
@@ -3,35 +3,35 @@ import type { OnenewsType } from '../types/mainPage';
 export const newsData: OnenewsType[] = [
     {
         "id": 1,
-        "time": "2025-08-06 09:20:00",
+        "time": "2025-08-06 09:20",
         "title": "Передчасно пішов із життя офіцер військової служби правопорядку зі Сміли",
         "link": "https://provce.ck.ua/peredchasno-pishov-iz-zhyttia-pratsivnyk-viyskovoi-sluzhby-pravoporiadku-zi-smily/",
         "isImportant": true
     },
     {
         "id": 2,
-        "time": "2025-08-06 08:45:00",
+        "time": "2025-08-06 08:45",
         "title": "У Резерв+ додали відстрочку для подружжя, в якому чоловік чи дружина мають інвалідність",
         "link": "https://provce.ck.ua/u-rezerv-dodaly-vidstrochku-dlia-podruzhzhia-v-iakomu-cholovik-chy-druzhyna-maiut-invalidnist/",
         "isImportant": false
     },
     {
         "id": 3,
-        "time": "2025-08-06 08:09:00",
+        "time": "2025-08-06 08:09",
         "title": "Понад 20 млрд грн сплатили платники Черкащини в бюджет за сім місяців",
         "link": "https://provce.ck.ua/ponad-20-mlrd-hrn-splatyly-platnyky-cherkashchyny-v-biudzhet-za-sim-misiatsiv/",
         "isImportant": false
     },
     {
         "id": 4,
-        "time": "2025-08-06 09:55:00",
+        "time": "2025-08-06 09:55",
         "title": "У Черкасах створять новий герб – старий асоціюється з росією",
         "link": "https://provce.ck.ua/u-cherkasakh-stvoriat-novyy-herb-staryy-asotsiiuietsia-z-praporom-rosii/",
         "isImportant": false
     },
     {
         "id": 5,
-        "time": "2025-08-06 10:31:00",
+        "time": "2025-08-06 10:31",
         "title": "Через недопалки за минулу добу на Черкащині вигоріло близько 7 гектарів стерні",
         "link": "https://provce.ck.ua/cherez-nedopalky-za-mynulu-dobu-na-cherkashchyni-vyhorilo-blyzko-7-hektariv-sterni/",
         "isImportant": false
